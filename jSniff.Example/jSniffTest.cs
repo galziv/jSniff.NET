f02505d4ab5bf20c2acc4bc77a287a43bcba5368
gz0220 <gz0220@gmail.com>
2015-07-19 21:29:47 +0300

    unit test update

--- jSniff.Example/jSniffTest.cs
+++ jSniff.Example/jSniffTest.cs
@@ -6,7 +6,7 @@ using OpenQA.Selenium.Chrome;
 namespace jSniff.Example
 {
     [TestClass]
-    public class UnitTest1
+    public class jSniffTest
     {
         private ChromeDriver driver = new ChromeDriver();
         private readonly string exmampleUrl = "file:///" + Environment.CurrentDirectory.Replace('\\', '/') + "/../../example.html";
@@ -14,19 +14,30 @@ namespace jSniff.Example
         [TestMethod]
         public void TestMethod1()
         {
+            // setup
             driver.Navigate().GoToUrl(exmampleUrl);
-
             Manager manager = new Manager(driver);
+            string a = "3";
+            string b = "7";
 
+            // execute
             manager.Sniffify("window", "multiply", "window_multiply", "function() { console.log('log from custom function');}");
+            driver.FindElement(By.Id("a")).Clear();
+            driver.FindElement(By.Id("a")).SendKeys(a);
+            driver.FindElement(By.Id("b")).Clear();
+            driver.FindElement(By.Id("b")).SendKeys(b);
             driver.FindElement(By.Id("execute")).Click();
-            driver.FindElement(By.Id("a")).SendKeys("1");
-            driver.FindElement(By.Id("b")).SendKeys("2");
             driver.FindElement(By.Id("execute")).Click();
-
             var invocations = manager.GetInvocations("window_multiply");
-            var last = manager.GetLastInvocation("window_multiply");
+            var lastInvocation = manager.GetLastInvocation("window_multiply");
             var parameters = manager.GetLastInvocationParams("window_multiply");
+
+            // expect
+            Assert.AreEqual(invocations.Length, 2);
+            Assert.AreEqual(lastInvocation.ExecutionParameters["a"], a, "last invocation a value is incorrect");
+            Assert.AreEqual(lastInvocation.ExecutionParameters["b"], b, "last invocation b value is incorrect");
+            Assert.AreEqual(parameters["a"], a,"parameters a value is incorrect");
+            Assert.AreEqual(parameters["b"], b,"parameters b value is incorrect");
         }
 
         // This closes the driver down after the test has finished.
